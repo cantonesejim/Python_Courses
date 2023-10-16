b10da50afe7defe692931fa247048c29ee6f5cfc
Matt A. <cantonesejim@gmail.com>
2023-10-16 18:51:31 -0500

    2nd Project from Beginner Ebook

---
+++ CompletePython_LearnByDoing2023/PythonForBeginnersEBookConcurrentExercises/02_Countries_Visited/Countries_Visited.py
@@ -0,0 +1,7 @@
+Countries_Visited = ["Argentina", "Mexico", "England", "Japan", "Nigeria", "South Korea", "New Zealand"]
+User_Country = input("What Country?: ")
+
+if User_Country in Countries_Visited:
+    print("I have visited this country.")
+else:
+    print("I have not visited this country.")
